5410976adb25a754ae746bf33e8dbc0d8993f88a
Corey Shupe <coreyshupe101@gmail.com>
2018-07-22 10:39:43 -0400

    Introduced math utils.

--- src/main/java/com/github/coreyshupe/projecteuler/p1to10/Problem3.java
+++ src/main/java/com/github/coreyshupe/projecteuler/p1to10/Problem3.java
@@ -1,5 +1,6 @@
 package com.github.coreyshupe.projecteuler.p1to10;
 
+import com.github.coreyshupe.projecteuler.MathUtils;
 import com.github.coreyshupe.projecteuler.Problem;
 import com.github.coreyshupe.projecteuler.ProjectEulerProblem;
 
@@ -16,31 +17,12 @@ public class Problem3 implements Problem<Long> {
     long testable = 600_851_475_143L;
     long output = -1;
     for (long i = 3; i * i < testable; i += 2) {
-      if (isPrime(i)) {
+      if (MathUtils.isPrime(i)) {
         if (testable % i == 0) {
           output = i;
         }
       }
     }
     return output;
-  }
-
-  /**
-   * Simple prime test. <br>
-   * Starting at 3 (all numbers coming in have the precondition of not being divisible by 2). <br>
-   * We add 2 to each only getting odds, checking if n is divisible by it, if so it cannot be prime.
-   * <br>
-   * Checking if i squared is less than n to stop unnecessary checking of higher numbers.
-   *
-   * @param n The number to check if prime.
-   * @return if the variable n is prime.
-   */
-  private boolean isPrime(long n) {
-    for (long i = 3; i * i < n; i += 2) {
-      if (n % i == 0) {
-        return false;
-      }
-    }
-    return true;
   }
 }
