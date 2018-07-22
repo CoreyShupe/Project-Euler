be6f1fe31ac3a463082c0c62704358c40952dfa2
Corey Shupe <coreyshupe101@gmail.com>
2018-07-22 10:51:25 -0400

    Optimized prime logic.

--- src/main/java/com/github/coreyshupe/projecteuler/MathUtils.java
+++ src/main/java/com/github/coreyshupe/projecteuler/MathUtils.java
@@ -1,40 +1,36 @@
 package com.github.coreyshupe.projecteuler;
 
-/**
- * Some simple math utilities to work with the {@link Problem}s.
- */
+/** Some simple math utilities to work with the {@link Problem}s. */
 public final class MathUtils {
 
-    /**
-     * Simple prime test. <br>
-     * Starting at 3 (all numbers coming in have the precondition of not being divisible by 2). <br>
-     * We add 2 to each only getting odds, checking if n is divisible by it, if so it cannot be prime.
-     * <br>
-     * Checking if i squared is less than n to stop unnecessary checking of higher numbers.
-     * <br>
-     * Any number between 0-3 are always prime.
-     *
-     * @param n The number to check if prime.
-     * @return if the variable n is prime.
-     */
-    public static boolean isPrime(long n) {
-        if(n < 0) {
-            n = -n;
-        }
-        if(n <= 3) {
-            return true;
-        }
-        if(n % 2 == 0) {
-            return false;
-        }
-        for (long i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
+  /**
+   * Simple prime test. <br>
+   * Starting at 3 (all numbers coming in have the precondition of not being divisible by 2). <br>
+   * We add 2 to each only getting odds, checking if n is divisible by it, if so it cannot be prime.
+   * <br>
+   * Checking if i squared is less than n to stop unnecessary checking of higher numbers. <br>
+   * Any number between 0-3 are always prime.
+   *
+   * @param n The number to check if prime.
+   * @return if the variable n is prime.
+   */
+  public static boolean isPrime(long n) {
+    if (n < 0) {
+      n = -n;
     }
+    if (n <= 3) {
+      return true;
+    }
+    if (n % 2 == 0) {
+      return false;
+    }
+    for (long i = 3; i * i <= n; i += 2) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-    private MathUtils() {
-    }
+  private MathUtils() {}
 }
